d9b91c4287bf57eefd03bcd60be4440cf23fe6de
Cristian <a@a.com>
2023-12-07 11:56:00 +0100

    Fix import typo and update route paths

--- Practica2/router.js
+++ Practica2/router.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { __dirname } from './dirname.js';
 import marcas from "./src/brandsService.js";
-import { getBrand } from './src/brandsService.js';
+import { getBrand} from './src/brandsService.js';
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -10,35 +10,20 @@ router.get("/", (req, res) => {
     });
 });
 
-/* router.get('/form_marca.html', (req, res) => {
-    res.render('brandForm', {
-        brandName: req.query.brand,
-        owner: req.query.owner,
-        year: req.query.year,
-        image: req.query.urlLogo,
-        color: req.query.brandColor,
-    });
-}); */
-
-/* router.post('/submitForm', (req, res) => {
-    res.render('prueba', {
-        brandName: req.body.brand,
-        owner: req.body.owner,
-        year: req.body.year,
-        image: req.body.urlLogo,
-        color: req.body.brandColor,
-    });
-}); */
-
-router.post('/submitForm', (req, res) => {
-    const formData = req.body;
-    res.render('prueba', formData);
+router.get('/marca/:brandname', (req, res) => {
+    let brandName = req.params.brandname;
+    let brandInfo = getBrand(brandName);
+    res.render("marca", brandInfo);
 });
 
 router.get('/marca/:brandname', (req, res) => {
     let brandName = req.params.brandname;
     let brandInfo = getBrand(brandName);
     res.render("marca", brandInfo);
+});
+
+router.get('/createYourBrand', (req, res) => {
+    let 
 });
 
 
