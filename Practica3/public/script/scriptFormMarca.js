44f61e0f6e8c04b48f197940ded686fc32b67d92
Cristian <a@a.com>
2024-01-08 18:39:54 +0100

    Update form styling and validation

--- Practica3/public/script/scriptFormMarca.js
+++ Practica3/public/script/scriptFormMarca.js
@@ -8,53 +8,48 @@ const brandName = document.getElementById("marca");
 const nameError = document.getElementById('errorNombre');
 const checkBrandName = () => {
     if (brandName.value.trim() == '') {
-        nameError.textContent = "El nombre no puede ser vacío";
+        nameError.textContent = "El nombre de la marca no puede estar vacía";
     }
     if (brandName.value.charAt(0) !== brandName.value.charAt(0).toUpperCase()) {
-        nameError.textContent = 'La primera letra del nombre tiene que ser mayúscula.';
+        nameError.textContent = 'La primera letra del nombre de la marca tiene que ser mayúscula.';
     } else {
         nameError.textContent = "";
     }
 }
 
-brandName.addEventListener('input', checkBrandName);
-document.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    checkBrandName();
-
-    const brandYear = document.getElementById("año");
-    const yearError = document.getElementById('errorAno');
-
-    if (brandYear.value = "") {
-        nameError.textContent = "El año no puede ser vacío";
+const brandYear = document.getElementById("año");
+const yearError = document.getElementById('errorAno');
+const checkBrandYear = () => {
+    if (brandYear.value == "") {
+        nameError.textContent = "El año de creación no puede ser vacío";
         return;
     } else if (brandYear.value < 1800 || brandYear.value > 2024) {
-        yearError.textContent = "El año tiene que estar comprendido entre 1800 y 2024";
+        yearError.textContent = "El año de creación de la marca tiene que estar comprendido entre 1800 y 2024";
     }
     else {
         nameError.textContent = "";
     }
+}
 
-    const brandFounder = document.getElementById("nombre");
-    const founderError = document.getElementById("errorFundador");
+const brandFounder = document.getElementById("nombre");
+const founderError = document.getElementById("errorFundador");
+const checkBrandFounder = () => {
 
     if (brandFounder.value.trim() == '') {
-        founderError.textContent = "El nombre no puede ser vacío";
+        founderError.textContent = "El nombre del fundador no puede ser vacío";
         return;
-    } else if (brandFounder.charAt(0) !== brandFounder.charAt(0).toUpperCase()) {
-        founderError.textContent = 'La primera letra del nombre tiene que ser mayúscula.';
+    }
+    if (brandFounder.value.charAt(0) !== brandFounder.value.charAt(0).toUpperCase()) {
+        founderError.textContent = 'La primera letra del nombre del fundador tiene que ser mayúscula.';
         return;
     } else {
         founderError.textContent = "";
     }
+}
 
-    const brandImage = document.getElementById("imagen");
-    const imageError = document.getElementById("errorImagen");
-
-
-    const brandWeb = document.getElementById("web");
-    const webError = document.getElementById("errorWeb");
-
+const brandWeb = document.getElementById("web");
+const webError = document.getElementById("errorWeb");
+const checkBrandWeb = () => {
     if (brandWeb.value.trim() == '') {
         urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
 
@@ -65,17 +60,38 @@ document.addEventListener("submit", async function (event) {
     } else {
         nameError.textContent = "";
     }
+}
 
-    const brandPlayers = document.getElementById("colaborators");
-    const errorPlayers = document.getElementById("errorPlayers");
+const brandPlayers = document.getElementById("colaborators");
+const errorPlayers = document.getElementById("errorPlayers");
+const checkBrandPlayers = () => {
 
     if (brandPlayers.value.trim() == '') {
         errorPlayers.textContent = "El nombre no puede ser vacío";
         return;
-    } else if (brandPlayers.charAt(0) !== brandPlayers.charAt(0).toUpperCase()) {
+    } else if (brandPlayers.value.charAt(0) !== brandPlayers.value.charAt(0).toUpperCase()) {
         errorPlayers.textContent = 'La primera letra del nombre tiene que ser mayúscula.';
         return;
     } else {
         errorPlayers.textContent = "";
     }
+}
+
+brandName.addEventListener('input', checkBrandName);
+/* brandYear.addEventListener('input', checkBrandYear); */
+brandFounder.addEventListener('input', checkBrandFounder);
+brandWeb.addEventListener('input', checkBrandWeb);
+brandPlayers.addEventListener('input', checkBrandPlayers);
+
+
+document.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    checkBrandName();
+    checkBrandYear();
+    checkBrandFounder();
+    checkBrandWeb();
+    checkBrandPlayers();
+
+    const brandImage = document.getElementById("imagen");
+    const imageError = document.getElementById("errorImagen");
 });
