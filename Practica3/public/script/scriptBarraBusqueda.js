ddebbdf03c8b4c24576396db4a2c6ba69275d03e
Juanmabm73 <146847555+Juanmabm73@users.noreply.github.com>
2024-01-13 18:26:06 +0100

    Refactor search functionality in scriptBarraBusqueda.js

--- Practica3/public/script/scriptBarraBusqueda.js
+++ Practica3/public/script/scriptBarraBusqueda.js
@@ -3,7 +3,7 @@ document.addEventListener("keyup", e => {
     if (e.target.matches("#buscador")) {
         document.querySelectorAll("img.nombreBusqueda").forEach(img => {
             const container = img.parentElement.parentElement; // Contenedor de la imagen
-            if (img.alt.toLowerCase().startsWith(e.target.value.toLowerCase())) {
+            if (img.alt.toLowerCase().includes(e.target.value.toLowerCase())) {
                 container.style.display = ''; // Muestra el contenedor de la marca
             } else {
                 container.style.display = 'none'; // Oculta el contenedor de la marca
@@ -11,6 +11,20 @@ document.addEventListener("keyup", e => {
         });
     }
 });
+
+document.addEventListener("keyup", e =>{
+    if (e.target.matches("#buscador")){
+        document.querySelectorAll('.pala').forEach(div =>{
+            
+            let nombrePala = div.querySelector('.parrafoPala').textContent.toLowerCase().trim();
+            if (nombrePala.toLowerCase().startsWith(e.target.value.toLowerCase())) {
+                div.style.display = '';
+            } else {
+                div.style.display = 'none';
+            }
+        })
+    }
+})
 
 // en caso de que el texto que se coloque en el input = nombreMarca te redirige a la pagina de la marca
 document.addEventListener("keydown", e => {
